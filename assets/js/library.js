183550402130947dbdd85162cc62f4f455ec9b38
Jonathan Yiv <JonathanYiv@gmail.com>
2018-02-18 18:07:03 -0500

    Replaced Examples with Javascript Rendering function to take books from an array and display them on the page

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,6 +1,6 @@
 let myLibrary = []
 
-function Book(author, title, pages, read) {
+function Book(title, author, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
@@ -13,12 +13,37 @@ function addBookToLibrary() {
 
 // loops through myLibrary and displays each book
 // on the page
-function render() {
+const libraryPage = document.querySelector("#library");
 
+function render() {
+  myLibrary.forEach(book => {
+    const row = document.createElement("tr");
+    row.classList.add("book");
+
+    const titleCell = row.insertCell();
+    const title = document.createTextNode(book.title);
+    titleCell.appendChild(title);
+
+    const authorCell = row.insertCell();
+    const author = document.createTextNode(book.author);
+    authorCell.appendChild(author);
+
+    const pagesCell = row.insertCell();
+    const pages = document.createTextNode(book.pages);
+    pagesCell.appendChild(pages);
+
+    const readStatusCell = row.insertCell();
+    const readStatusButton = document.createElement("button");
+    const readStatus = document.createTextNode(book.read ? "Yes" : "No");
+    readStatusButton.appendChild(readStatus);
+    readStatusCell.appendChild(readStatusButton);
+
+    libraryPage.appendChild(row);
+  });
 }
 
 Book.prototype.toggleReadStatus = function() {
-
+  this.read = !this.read;
 }
 
 // Hint: You will need to associate your
@@ -26,3 +51,17 @@ Book.prototype.toggleReadStatus = function() {
 // in some way. One easy solution is giving
 // them a data-attribute that corresponds
 // to the index of the library array.
+
+const philosophersStone = new Book("Harry Potter and the Philosopher's Stone",
+                                   "J.K. Rowling",
+                                   223,
+                                   true);
+
+const chamberOfSecrets = new Book("Harry Potter and the Chamber of Secrets",
+                                  "J.K. Rowling",
+                                  251,
+                                  true);
+
+myLibrary.push(philosophersStone, chamberOfSecrets);
+
+render();
